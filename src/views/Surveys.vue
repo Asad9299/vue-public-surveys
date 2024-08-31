8aa8fe651237fd68ac9431dd1b26da731354d3b1
asad.m <asadm.spaceo@gmail.com>
2024-08-31 18:21:01 +0530

    PageComponent Added

--- src/views/Surveys.vue
+++ src/views/Surveys.vue
@@ -1,14 +1,9 @@
 <template>
-    <div class="min-h-full">
-        <header class="bg-white shadow">
-            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <h1 class="text-3xl font-bold tracking-tight text-gray-900">Surveys</h1>
-            </div>
-        </header>
-        <main>
-            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <!-- Your content -->
-            </div>
-        </main>
-    </div>
+    <PageComponent title="Surveys">
+        Surveys Content!!
+    </PageComponent>
 </template>
+
+<script setup lang="ts">
+    import PageComponent from '../components/PageComponent.vue';
+</script>
