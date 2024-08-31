81f4b95d86a652dcdcc6ee1cafe5108e3b01b47f
asad.m <asadm.spaceo@gmail.com>
2024-08-31 18:11:27 +0530

    Layouts & Unwanted Code Removed

---
+++ src/views/Surveys.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="min-h-full">
+        <header class="bg-white shadow">
+            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            <h1 class="text-3xl font-bold tracking-tight text-gray-900">Surveys</h1>
+            </div>
+        </header>
+        <main>
+            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            <!-- Your content -->
+            </div>
+        </main>
+    </div>
+</template>
