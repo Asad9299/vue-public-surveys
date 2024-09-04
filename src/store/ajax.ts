47ae8035f4421c024ac77fc7ac1740463c10816f
asad.m <asadm.spaceo@gmail.com>
2024-09-04 09:03:09 +0530

    Ajax and User Stores Added

---
+++ src/store/ajax.ts
@@ -0,0 +1,36 @@
+import axios, { AxiosResponse } from "axios";
+import { userStore, user } from "./user";
+
+export default class ajax {
+    
+    user: user;
+    headers: Record<string, string> =  {};
+    APP_API_URL: String = import.meta.env.VITE_API_URL;
+
+    constructor() {
+        this.setHeader('Accept', 'application/json');
+        this.user = userStore();
+        if (this.user.isLoggedIn) {
+            this.setHeader('Authorization', this.getAuthBearerHeader())
+        }
+    }
+
+    setHeader(key: string, value: string) : void {
+        this.headers[key] = value;
+    }
+
+    getAuthBearerHeader() : string {
+        return `Bearer ${this.user.authToken}`;
+    }
+
+    async get<T>(endpoint: string): Promise<AxiosResponse<T>> {
+        let response = await axios.get(this.APP_API_URL + endpoint, { headers: this.headers });
+        return response;
+    }
+
+    async post<T>(endpoint: string, data: T): Promise<AxiosResponse> {
+        let response = await axios.post(this.APP_API_URL + endpoint, data , { headers: this.headers });
+        return response;
+    }
+
+}
--- src/store/user.ts
+++ src/store/user.ts
@@ -3,11 +3,12 @@ import { ref } from "vue";
 
 export type user = {
   authToken?: string,
-  }
+  isLoggedIn: boolean
+}
 
 export const userStore = defineStore('user', () => {
-    const authToken = ref('asad');
-  
-    return { authToken }
-
+    const authToken = ref('asad'); 
+    const isLoggedIn = ref(true);
+    
+    return { authToken, isLoggedIn }
 })
