3eb8539b5a3cb7a099a63c3288e2c75c155935fa
asad.m <asadm.spaceo@gmail.com>
2024-09-08 18:37:14 +0530

    Registeration with serverside validations, pinia set and toastification messages

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import Toast from 'vue-toastification'
+import 'vue-toastification/dist/index.css'
+
 import  router  from './router';
 import './style.css'
 import App from './App.vue'
@@ -7,5 +10,5 @@ import App from './App.vue'
 const app = createApp(App);
 app.use(createPinia());
 app.use(router);
-
+app.use(Toast);
 app.mount('#app')
--- src/store/user.ts
+++ src/store/user.ts
@@ -1,14 +1,35 @@
 import { defineStore } from "pinia";
-import { ref } from "vue";
+import { ref, reactive } from "vue";
 
 export type user = {
+  name: string,
+  email: string,
+  password?: string,
+  password_confirmation?: string,
   authToken?: string,
-  isLoggedIn: boolean
+  isLoggedIn?: boolean
 }
 
 export const userStore = defineStore('user', () => {
-    const authToken = ref('asad'); 
-    const isLoggedIn = ref(true);
+    // const authToken = ref(''); 
+    // const isLoggedIn = ref(true);
+
+    const userData: user = reactive({
+        name: '',
+        email: '',
+        authToken: '',
+        isLoggedIn: false,
+      });
+
+    const setUser = (userObj: user) => {
+        userData.name = userObj.name;
+        userData.email = userObj.email;
+        userData.authToken = userObj.authToken;
+        userData.isLoggedIn = userObj.isLoggedIn;
+        
+        console.log('set user data', userData);
+        sessionStorage.setItem('user', JSON.stringify(userData));
+    }
     
-    return { authToken, isLoggedIn }
+    return { userData, setUser }
 })
