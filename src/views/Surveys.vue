9db8ee0d3dc6f22830846f15a607e4607538aa87
Asad Mansuri <asadmansuri6797@gmail.com>
2024-09-15 15:48:21 +0530

    Delete Survey Button Added

--- src/views/Surveys.vue
+++ src/views/Surveys.vue
@@ -62,11 +62,30 @@
             </svg>
             Edit
           </router-link>
-        </div>
+          
+          <!-- Delete Button -->
+          <button 
+            @click="deleteSurvey(survey)" 
+            class="flex items-center text-red-600 hover:text-red-800 focus:outline-none"
+            aria-label="Delete Survey"
+          >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-trash h-5 w-5"
+            viewBox="0 0 16 16"
+          >
+            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+            <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+          </svg>
+        </button>  
       </div>
     </div>
+  </div>
        
-    </PageComponent>
+  </PageComponent>
 </template>
 
 <script setup lang="ts">
@@ -78,4 +97,9 @@
 
     const surveys = ref<Survey[]>(surveyStoreObj.surveys);
 
+    const deleteSurvey = (survey: Survey) => {
+      if (confirm('Are you sure you want to delete this Survey?')) {
+        // @TODO: Delete Survey Code goes here.
+      }
+    }
 </script>
