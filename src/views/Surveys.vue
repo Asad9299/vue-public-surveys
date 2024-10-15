d960922300fb3cac6ec5f9b636ec9677c73e8dd3
Asad Mansuri <asadmansuri6797@gmail.com>
2024-10-15 08:23:59 +0530

    Survey List Pagination Added

--- src/views/Surveys.vue
+++ src/views/Surveys.vue
@@ -99,31 +99,31 @@
 
 
           </div>
-          <div class="flex justify-center mt-5">
-        <nav
-          class="relative z-0 inline-flex justify-center rounded-md shadow-sm -space-x-px"
-          aria-label="Pagination"
-        >
+          <div class="flex justify-center mt-5" v-if="surveyStoreObj.surveys.length > 0">
+            <nav
+              class="relative z-0 inline-flex justify-center rounded-md shadow-sm -space-x-px"
+              aria-label="Pagination"
+            >
 
-        <a
-          v-for="(link, i) of surveyStoreObj.paginateLinks.links"
-          :key="i"
-          :disabled="!link.url"
-          href="#"
-          @click="getForPage($event, link)"
-          aria-current="page"
-          class="relative inline-flex items-center px-4 py-2 border text-sm font-medium whitespace-nowrap"
-          :class="[
-            link.active
-              ? 'z-10 bg-indigo-50 border-indigo-500 text-indigo-600'
-              : 'bg-white border-gray-300 text-gray-500 hover:bg-gray-50',
-            i === 0 ? 'rounded-l-md bg-gray-100 text-gray-700' : '',
-            i === surveyStoreObj.paginateLinks.links.length - 1 ? 'rounded-r-md' : '',
-          ]"
-          v-html="link.label"
-        >
-        </a>
-      </nav>
+            <a
+              v-for="(link, i) of surveyStoreObj.paginateLinks.links"
+              :key="i"
+              :disabled="!link.url"
+              href="#"
+              @click="getForPage($event, link)"
+              aria-current="page"
+              class="relative inline-flex items-center px-4 py-2 border text-sm font-medium whitespace-nowrap"
+              :class="[
+                link.active
+                  ? 'z-10 bg-indigo-50 border-indigo-500 text-indigo-600'
+                  : 'bg-white border-gray-300 text-gray-500 hover:bg-gray-50',
+                i === 0 ? 'rounded-l-md bg-gray-100 text-gray-700' : '',
+                i === surveyStoreObj.paginateLinks.links.length - 1 ? 'rounded-r-md' : '',
+              ]"
+              v-html="link.label"
+            >
+            </a>
+          </nav>
     </div>
   </div>
   </PageComponent>
@@ -166,8 +166,11 @@
             surveyStoreObj.surveys = surveyStoreObj.surveys.filter((sr) => {
               return sr.id !== survey.id 
             });
-            toast.success('Survey has been deleted successfully');
-            
+            // redirect to page 1, if all the records of that page are deleted.
+            if ( surveyStoreObj.surveys.length === 0 ) {
+              await surveyStoreObj.surveyList();
+            }
+            toast.success('Survey has been deleted successfully'); 
           }
         }
       } catch ( error:any ) {
